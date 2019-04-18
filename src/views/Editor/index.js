086799b5f01d7a602f02e848f7a861a57f70654a
da07ng <gaodaojing@gmail.com>
2019-04-18 17:47:27 +0800

    update

--- src/views/Editor/index.js
+++ src/views/Editor/index.js
@@ -8,13 +8,100 @@ const initialValue = Value.fromJSON({
       {
         object: 'block',
         type: 'paragraph',
-        key: '123456',
         nodes: [
           {
             object: 'text',
             leaves: [
               {
-                text: 'A line of text in a paragraph.'
+                text: 'This is editable '
+              },
+              {
+                text: 'rich',
+                marks: [
+                  {
+                    type: 'bold'
+                  }
+                ]
+              },
+              {
+                text: ' text, '
+              },
+              {
+                text: 'much',
+                marks: [
+                  {
+                    type: 'italic'
+                  }
+                ]
+              },
+              {
+                text: ' better than a '
+              },
+              {
+                text: '<textarea>',
+                marks: [
+                  {
+                    type: 'code'
+                  }
+                ]
+              },
+              {
+                text: '!'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text:
+                  "Since it's rich text, you can do things like turn a selection of text "
+              },
+              {
+                text: 'bold',
+                marks: [
+                  {
+                    type: 'bold'
+                  }
+                ]
+              },
+              {
+                text:
+                  ', or add a semantically rendered block quote in the middle of the page, like this:'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        object: 'block',
+        type: 'block-quote',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A wise quote.'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'Try it out for yourself!'
               }
             ]
           }
@@ -37,8 +124,75 @@ class Editor extends Component {
     };
   }
 
+  onKeyDown(event, editor, next) {
+    if (!event.ctrlKey) return next();
+
+    switch (event.key) {
+      case 'b': {
+        event.preventDefault();
+        editor.toggleMark('bold');
+        break;
+      }
+      case '`': {
+        const isCode = editor.value.blocks.some(block => block.type === 'code');
+        event.preventDefault();
+        editor.setBlocks(isCode ? 'paragraph' : 'code');
+        break;
+      }
+      default: {
+        return next();
+      }
+    }
+  }
+
+  renderNode(props, editor, next) {
+    const { attributes, children, node } = props;
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>;
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>;
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>;
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>;
+      case 'list-item':
+        return <li {...attributes}>{children}</li>;
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>;
+      default:
+        return next();
+    }
+  }
+
+  renderMark(props, editor, next) {
+    const { children, mark, attributes } = props;
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>;
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+      case 'italic':
+        return <em {...attributes}>{children}</em>;
+      case 'underlined':
+        return <u {...attributes}>{children}</u>;
+      default:
+        return next();
+    }
+  }
+
   render() {
-    return <SlateEditor value={this.state.value} onChange={this.onChange} />;
+    return (
+      <SlateEditor
+        value={this.state.value}
+        onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
+        renderNode={this.renderNode}
+        renderMark={this.renderMark}
+      />
+    );
   }
 }
 
